256d694cee4cfa6236cc998907433458bf29ffa4
Marko <mmedojevicbg@gmail.com>
2016-01-23 13:20:22 +0100

    form action bug

--- ImageUploadAction.php
+++ ImageUploadAction.php
@@ -10,7 +10,7 @@ class ImageUploadAction extends Action
     {
         $sourcePath = $_FILES['file']['tmp_name'];
         if(getimagesize($sourcePath)) {
-            $targetPath = $this->uploadsPath = DIRECTORY_SEPARATOR . $_FILES['file']['name'];
+            $targetPath = $this->uploadsPath . DIRECTORY_SEPARATOR . $_FILES['file']['name'];
             move_uploaded_file($sourcePath, $targetPath) ;
             $response = ['status' => 1,
                          'message' => 'Image has been uploaded successfully.'];
